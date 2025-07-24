4e5a4ec639099de5c89a886b5d074b89a2aa5c72
Rudra007 <rudrakshkachhawa@gmail.com>
2025-07-24 19:39:24 +0530

    use proxy package

--- src/screens/loginScreen.tsx
+++ src/screens/loginScreen.tsx
@@ -1,14 +1,14 @@
 // LoginScreen.tsx
 import React from 'react';
 import { View } from 'react-native';
-import { ShowGoogleLoginButton } from 'react-native-proxy'
+import {ShowProxyAuth} from '@msg91comm/react-native-36blocks-proxy'
 import { setUserInfo } from '../redux/features/userInfo/userInfoSlice';
 import { store } from '../redux/store';
 
 const LoginScreen = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <ShowGoogleLoginButton config={{}}
+            <ShowProxyAuth config={{}}
                 referenceId='870623a1697443499652ceeab330e5'
                 onLoginSuccess={(data) => {
                     store.dispatch(setUserInfo({ proxyAuthToken: data.proxy_auth_token }));
